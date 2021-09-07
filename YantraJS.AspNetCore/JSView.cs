2ffe138281e8b5cb23957be60e04e8e33b28881f
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-09-07 14:05:36 +0530

    wip

--- YantraJS.AspNetCore/JSView.cs
+++ YantraJS.AspNetCore/JSView.cs
@@ -1,16 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using YantraJS.Core;
+using YantraJS.Core.Clr;
 
 namespace YantraJS.AspNetCore
 {
     public class JSView : IView
     {
-        static KeyString model = "model";
-        static KeyString render = "render";
+        static KeyString model = nameof(model);
+        static KeyString render = nameof(render);
+        static KeyString services = nameof(services);
+        static KeyString viewContext = nameof(viewContext);
 
         readonly string filePath;
         readonly string folder;
@@ -36,12 +42,42 @@ namespace YantraJS.AspNetCore
         {
             using (var yc = new YantraContext(this.folder))
             {
+                var ys = new YantraServiceResolver(context.HttpContext.RequestServices).Marshal();
+                yc[services] = ys;
                 var text = await yc.RunAsync(folder, "./" + filePath);
                 var view = text[KeyStrings.@default].CreateInstance();
                 view[model] = context.ViewData.Model.Marshal();
+                view[services] = ys;
+                view[viewContext] = context.Marshal();
                 var data = view.InvokeMethod(render).ToString();
                 context.Writer.WriteLine(data);
             }
         }
+    }
+
+    public class YantraServiceResolver 
+    {
+        private readonly IServiceProvider services;
+
+        public YantraServiceResolver(IServiceProvider services)
+        {
+            this.services = services;
+        }
+
+        public JSValue Resolve(in Arguments a)
+        {
+            var f = a[0] ?? throw new ArgumentNullException();
+            if(!f.ConvertTo<Type>(out var type))
+            {
+                if (!f.IsString)
+                    throw new ArgumentOutOfRangeException();
+                var typeName = f.ToString();
+                type = Type.GetType(typeName) ?? throw new TypeAccessException($"Could not load type `{typeName}`");
+                
+            }
+            return services.GetRequiredService(type).Marshal();
+        }
+
+
     }
 }
--- YantraJS.WebSite/Views/Home/Index.js
+++ YantraJS.WebSite/Views/Home/Index.js
@@ -1,6 +1,13 @@
-﻿export default class IndexView {
+﻿// const clr = require("clr").default;
+
+// const markdownService = clr.getClass("");
+
+export default class IndexView {
 
     render() {
+
+        // const serivce = this.services.resolve("....");
+
         return `Demo ${this.model.message}`;
     }
 
