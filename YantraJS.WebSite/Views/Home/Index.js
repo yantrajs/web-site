a66284505dccceeb51d91b60d00e012e563f0619
Akash Kava <ackava@gmail.com>
2021-11-08 17:15:13 +0530

    Style updated

--- YantraJS.WebSite/Views/Home/Index.js
+++ YantraJS.WebSite/Views/Home/Index.js
@@ -22,12 +22,17 @@ export default class IndexView extends View {
         const content = await ms.processMarkdownAsync("https://raw.githubusercontent.com/yantrajs/yantra/main/README.md");
 
         return `
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <title>YantraJS JavaScript Engine written in .NET Standard</title>
+    <meta charset="utf-8">
     <style>
     * {
         font-family: -apple-system, BlinkMacSystemFont, "Segoe UI Variable", "Segoe UI", system-ui, ui-sans-serif, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"
+    }
+    body {
+        overflow-x: hidden;
     }
     button {
         border-radius: 5px;
@@ -39,27 +44,67 @@ export default class IndexView extends View {
         padding-left: 10px;
         padding-right: 10px;
     }
+      .background--custom {
+        background-color: #FFFFFF;
+        width: 100vw;
+        height: 100vh;
+        position: absolute;
+        overflow: hidden;
+        z-index: -2;
+        top: 0;
+        left: 0;
+      }
+      canvas#canvas {
+        z-index: -1;
+        position: absolute;
+        width: 100%;
+        height: 60%;
+        transform: rotate(-12deg) scale(2) translateY(-56%);
+        --gradient-color-1: #EF008F;
+        --gradient-color-2: #F46E6E;
+        --gradient-color-3: #7038ff;
+        --gradient-color-4: #A427FF;
+        --gradient-speed: 0.000006;
+      }
     </style>
 </head>
 <body>
-    <h1>YantraJS (Machine in Sanskrit)</h1>
-    <p>
-        Please visit GitHub repo at <a href="https://github.com/yantrajs/yantra">YantraJS</a>
-        <ul>
-            <li><a href="https://github.com/yantrajs/yantra/discussions">Discussions</a></li>
-            <li><a href="https://github.com/yantrajs/yantra/issues">Issues</a></li>
-            <li><a href="https://github.com/yantrajs/yantra/wiki">Documentation</a></li>
-        </ul>
-    </p>
-    <p>
-        ${content}
-    </p>
-    <p>
-        <h3>Sponsor - Web Atoms (React Native alike) for Xamarin Forms</h3>
-        <p>This project is supported by Web Atoms, Web Atoms provides JSX+JavaScript bridge for Xamarin Forms 
-        which lets you write Xamarin Forms apps in React Native style.</p>
-        <a href="https://www.webatoms.in" target="_blank"><img src="https://cdn.jsdelivr.net/gh/web-atoms/resources@v1.0.1/ads/landscape/JSX-for-Xamarin.Forms.gif" style="border:none"/></a>
-    </p>
+    <div class="background--custom">
+      <canvas id="canvas" />
+    </div>
+    <div style="width: 700px; margin-left: auto; margin-top:20px; margin-bottom: 20px; margin-right: auto; background-color: #cbcbe35e; padding: 20px; border-radius: 20px;">
+        <h1>YantraJS (Machine in Sanskrit)</h1>
+        <p>
+            Please visit GitHub repo at <a href="https://github.com/yantrajs/yantra">YantraJS</a>
+            <ul>
+                <li><a href="https://github.com/yantrajs/yantra/discussions">Discussions</a></li>
+                <li><a href="https://github.com/yantrajs/yantra/issues">Issues</a></li>
+                <li><a href="https://github.com/yantrajs/yantra/wiki">Documentation</a></li>
+            </ul>
+        </p>
+        <p>
+            ${content}
+        </p>
+        <div style="width: 700px">
+            <h3>Support YantraJS</h3>
+            <a href="https://patreon.com/yantrajs">patreon.com/yantrajs</a>
+        </div>
+        <div style="width: 700px">
+            <h3>Sponsor - Web Atoms (React Native alike) for Xamarin Forms</h3>
+            <p>This project is supported by Web Atoms, Web Atoms provides JSX+JavaScript bridge for Xamarin Forms 
+            which lets you write Xamarin Forms apps in React Native style.</p>
+            <div style="margin-left: auto; margin-right: auto; width: 650px;">
+                <a href="https://www.webatoms.in"
+                   style="text-align: center"
+                   target="_blank"><img src="https://cdn.jsdelivr.net/gh/web-atoms/resources@v1.0.1/ads/landscape/JSX-for-Xamarin.Forms.gif" style="border:none; width: 650px;"/></a>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/gh/greentfrapp/pocoloco@minigl/minigl.js"></script>
+    <script>
+    var gradient = new Gradient();
+    gradient.initGradient("#canvas");
+    </script>
 </body>
 </html>
 `;
